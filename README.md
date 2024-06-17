99f79f4d412e71bb9b64bcc24d240d2f9b08d965
Via Scientiae <144046360+viascientiae@users.noreply.github.com>
2024-06-17 15:44:48 +0530

    Finish working and refactored palindrome method

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a Phrase object with the Palindrome detector
+
+This is a sample NPM module created in the Learn Enough JavaScript to Be Dangerous tutorial created by Michael Hartl.
--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-//Reverses a string.
-function reverse(string) {
-  return Array.from(string).reverse().join("");
+module.exports = Phrase;
+
+// Adds `reverse` to all strings.
+String.prototype.reverse = function() {
+  return Array.from(this).reverse().join("");
 }
 
-//Defines a Phrase object.
+// Defines a Phrase object.
 function Phrase(content) {
   this.content = content;
 
-  this.processor = function(string) {
-    return string.toLowerCase();
-  }
-  
-  //Returns content processed for palindrome testing.
+  // Returns content processed for palindrome testing.
   this.processedContent = function processedContent() {
-    return this.processor(this.content);
+    return this.letters().toLowerCase();
   }
 
-  //Returns true for a palindrome, false otherwise.
+  // Returns the letters in the content.
+  // For example:
+  //   new Phrase("Hello, world!").letters() === "Helloworld"
+  this.letters = function letters() {
+    return (this.content.match(/[a-z]/gi) || []).join("");
+  }
+
+  // Returns true if the phrase is a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    return this.processedContent === reverse(this.processedContent);
+    return this.processedContent() === this.processedContent().reverse();
   }
 }
-
-//Defines a TranslatedPhrase object.
-function TranslatedPhrase(content, translation) {
-  this.content = content;
-  this.translation = translation;
-}
-
-TranslatedPhrase.prototype = new Phrase();
